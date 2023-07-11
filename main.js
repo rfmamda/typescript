55092b4aa4218ea3296ea7204ce53c947f25bcb7
rfmamda <ramshaferoz1993@gmail.com>
2023-07-12 03:02:08 +0500

    my first typescript assigment

---
+++ main.js
@@ -0,0 +1,45 @@
+// Task 2
+var person_name = "Eric";
+console.log("hello " + person_name + "! would you like to learn some python today?");
+//Task 2
+//Task 3
+var myname = "ramsha Owais";
+var lowercase_name;
+var uppercase_name;
+var titlecase_name;
+var str = "abcdefghijkl";
+lowercase_name = myname.toLowerCase();
+console.log("lowercase name = " + lowercase_name);
+uppercase_name = myname.toUpperCase();
+console.log("uppercase name = " + uppercase_name);
+titlecase_name = myname[0].toUpperCase() + myname.substr(1);
+console.log("titlecase name = " + titlecase_name);
+//Task 3
+//Task 4
+console.log("Quaid e Azam once said," + "\"" + "with Unity, Faith and Discipline we will compare with any nation of the world." + "\"");
+//Task 4
+//Task 5
+var famous_person = "Quaid e Azam";
+var famous_quote = "with Unity, Faith and Discipline we will compare with any nation of the world.";
+console.log(famous_person + " once said," + "\"" + famous_quote + "\"");
+//Task 5
+//Task 6
+var my_name = "\t" + "ramsha" + "\t";
+console.log(my_name);
+var strip_name = my_name.trim();
+console.log(strip_name);
+//Task 6
+//Task 7 & 8
+var add = 4 + 4;
+var sub = 12 - 4;
+var mul = 4 * 2;
+var div = 16 / 2;
+console.log("4+4= " + add);
+console.log("12-4= " + sub);
+console.log("4*2= " + mul);
+console.log("16/2= " + div);
+//Task 7 & 8;
+//Task 9
+var myfav_num = 0;
+var msg = "my favourite number is " + myfav_num;
+console.log(msg);
---
+++ main.ts
@@ -0,0 +1,51 @@
+// Task 2
+let person_name="Eric";
+console.log("hello "+person_name+"! would you like to learn some python today?");
+//Task 2
+
+//Task 3
+let myname="ramsha Owais";
+let lowercase_name;
+let uppercase_name;
+let titlecase_name;
+let str="abcdefghijkl";
+lowercase_name=myname.toLowerCase();
+console.log("lowercase name = "+lowercase_name);
+uppercase_name=myname.toUpperCase();
+console.log("uppercase name = "+uppercase_name);
+titlecase_name=myname[0].toUpperCase()+myname.substr(1);
+console.log("titlecase name = "+titlecase_name);
+//Task 3
+
+//Task 4
+console.log("Quaid e Azam once said,"+"\""+"with Unity, Faith and Discipline we will compare with any nation of the world."+"\"");
+//Task 4
+
+//Task 5
+let famous_person="Quaid e Azam";
+let famous_quote="with Unity, Faith and Discipline we will compare with any nation of the world.";
+console.log(famous_person+" once said,"+"\""+famous_quote+"\"");
+//Task 5
+
+//Task 6
+let my_name="\t"+"ramsha"+"\t";
+console.log(my_name);
+let strip_name=my_name.trim();
+console.log(strip_name);
+//Task 6
+
+//Task 7 & 8
+let add=4+4;
+let sub=12-4;
+let mul=4*2;
+let div=16/2;
+console.log("4+4= "+add);
+console.log("12-4= "+sub);
+console.log("4*2= "+mul);
+console.log("16/2= "+div);
+//Task 7 & 8;
+
+//Task 9
+let myfav_num=0;
+let msg="my favourite number is "+myfav_num;
+console.log(msg);
